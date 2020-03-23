b2e56b8871dd314f7c6336c73dffff404b755cc8
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-03-23 23:59:52 +0100

    Create guest landing page #1

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'))
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+    res.render("home-guest");
+});
+
+app.listen(3000);
