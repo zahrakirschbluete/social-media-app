93bf3c3dfa64de503b4a09c2374bd8be78bff418
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-03-26 13:08:00 +0100

    Set up controllers #6

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = () => {
+
+}
+
+exports.logout = () => {
+    
+}
+
+exports.register = (req, res) => {
+    res.send("Thanks for trying to register.")
+}
+
+exports.home = (req, res) => {
+    res.render('home-guest')
+}
--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('./controllers/userController')
 
-router.get('/', (req, res) => {
-    res.render('home-guest')
-})
+router.get('/', userController.home)
+router.post('/register', userController.register)
 
 module.exports = router
