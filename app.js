1078cf88a61e77dc34b15632ab8e2d4a57b540ce
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-03-28 19:54:28 +0100

    Connect to mongodb database

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-const router = require('./router')
+const router = require("./router");
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.use('/', router)
+app.use("/", router);
 
-app.listen(3000);
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,16 @@
+const {
+    CONNECTION_STRING
+} = require('./cs.config')
+
+const mongodb = require('mongodb');
+mongodb.connect(
+    CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err, client) => {
+        module.exports = client.db();
+        const app = require("./app");
+        app.listen(3000);
+    }
+);
