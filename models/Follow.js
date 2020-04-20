ad4c3b2286c03eab2035e2c7d775eb026306f973
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-04-20 22:32:11 +0200

    Create followers screen

--- models/Follow.js
+++ models/Follow.js
@@ -1,6 +1,7 @@
 const usersCollection = require('../db').db().collection("users")
 const followsCollection = require('../db').db().collection("follows")
 const ObjectID = require('mongodb').ObjectID
+const User = require('./User')
 
 let Follow = function (followedUsername, authorId) {
     this.followedUsername = followedUsername
@@ -87,6 +88,47 @@ Follow.isVisitorFollowing = async function (followedId, visitorId) {
     } else {
         return false
     }
+}
+
+Follow.getFollowersById = function (id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let followers = await followsCollection.aggregate([{
+                    $match: {
+                        followedId: id
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "authorId",
+                        foreignField: "_id",
+                        as: "userDoc"
+                    }
+                },
+                {
+                    $project: {
+                        username: {
+                            $arrayElemAt: ["$userDoc.username", 0]
+                        },
+                        email: {
+                            $arrayElemAt: ["$userDoc.email", 0]
+                        }
+                    }
+                }
+            ]).toArray()
+            followers = followers.map(function (follower) {
+                let user = new User(follower, true)
+                return {
+                    username: follower.username,
+                    avatar: user.avatar
+                }
+            })
+            resolve(followers)
+        } catch {
+            reject()
+        }
+    })
 }
 
 module.exports = Follow
--- router.js
+++ router.js
@@ -12,7 +12,7 @@ router.post('/logout', userController.logout)
 
 //profile related routes
 router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
-
+router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
