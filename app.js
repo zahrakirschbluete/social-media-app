cfb95ccc586450fde423d40268e33c9116ed3637
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-04-24 21:22:12 +0200

    Create two way live chat

--- app.js
+++ app.js
@@ -58,4 +58,34 @@ app.set("view engine", "ejs");
 
 app.use("/", router);
 
-module.exports = app;
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+io.use(function (socket, next) {
+  sessionOptions(socket.request, socket.request.res, next)
+})
+
+io.on('connection', function (socket) {
+  if (socket.request.session.user) {
+    let user = socket.request.session.user
+
+    socket.emit('welcome', {
+      username: user.username,
+      avatar: user.avatar
+    })
+
+    socket.on('chatMessageFromBrowser', (data) => {
+      socket.broadcast.emit('chatMessageFromServer', {
+        message: sanitizteHTML(data.message, {
+          allowedTags: [],
+          allowedAttributes: {}
+        }),
+        username: user.username,
+        avatar: user.avatar
+      })
+    })
+  }
+})
+
+module.exports = server;
